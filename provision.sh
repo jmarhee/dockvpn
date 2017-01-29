240aec13fe759bcf6138663b1fdc66052cc442b8
Joseph D. Marhee <jmarhee@interiorae.com>
2017-01-29 13:29:33 -0700

    adding user-data script

---
+++ provision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function install_pkgs() {
+    apt-get update && \
+    apt-get install -y git-core curl
+}
+
+function install_compose() {
+    curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+    chmod +x /usr/local/bin/docker-compose
+}
+
+function deploy_vpn() {
+    git clone https://github.com/jmarhee/dockvpn.git && \
+    cd dockvpn && \
+    docker-compose up -d
+}
+
+install_pkgs && install_compose && deploy_vpn
