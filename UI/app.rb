20d20b6bb2acf3cbcc87ab6049f8caecde6cd7b1
Joseph D. Marhee <jmarhee@interiorae.com>
2017-02-14 23:45:20 -0700

    updating UI, adding thin to deal with URI handling from provisioner

--- UI/app.rb
+++ UI/app.rb
@@ -3,10 +3,10 @@ require 'sinatra'
 require 'send_file'
 
 get "/" do
-	body "<h2>Single Use Download</h2><p>All subsequent requests will fail, and a new VPN server will need to be provisioned in order to regenerate.</p><a href='/config' download='client.ovpn'>Download Config</a>"
+	erb :index
 end
 
-get "/config" do  
+get "/config" do
 	response = HTTParty.get("http://dockvpn_serveconfig_1:8080")
     tempfile = Tempfile.new('client.ovpn')
     File.open(tempfile.path,'w') do |f|
