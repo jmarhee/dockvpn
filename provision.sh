a4ecf22060eb35ed03284befd221a78054396282
Joseph D. Marhee <joseph@marhee.me>
2017-08-20 02:42:52 -0600

    updates prep for ipv6 on docker host

--- provision.sh
+++ provision.sh
@@ -10,10 +10,19 @@ function install_compose() {
     chmod +x /usr/local/bin/docker-compose
 }
 
+function ipv6_prep () {
+    sed -i 's/DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"/DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4 --ipv6 --fixed-cidr-v6=2001:db8:1::\/64"/g' /etc/default/docker && \
+    service docker restart && \
+    ip -6 route add 2001:db8:1::/64 dev docker0 && \
+    sysctl net.ipv6.conf.default.forwarding=1 && \
+    sysctl net.ipv6.conf.all.forwarding=1 && \
+    sysctl net.ipv6.conf.eth0.accept_ra=2
+}
+
 function deploy_vpn() {
     git clone https://github.com/jmarhee/dockvpn.git && \
     cd dockvpn && \
     docker-compose up -d
 }
 
-install_pkgs && install_compose && deploy_vpn
+install_pkgs && install_compose && ipv6_prep && deploy_vpn
