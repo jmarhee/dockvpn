823e555d59753271c53172bf59ac1ca5dc578925
Joseph D. Marhee <jmarhee@interiorae.com>
2020-11-20 03:07:11 -0600

    Makes image reusable on each run, updates compose file and provision.sh to reflect this. Bumps compose version to 1.27.4

--- provision.sh
+++ provision.sh
@@ -19,7 +19,7 @@ function install_pkgs() {
 }
 
 function install_compose() {
-    curl -L "https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+    curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
 }
 
